645d0f10db6b2ef3ac94f284f918fb4e8b37032a
sunzhl <18611822573@163.com>
2015-10-22 17:39:41 +0800

    提交第一版，已经实现基本应用，无问题

---
+++ react-bootstrap-demo/js/app.js
@@ -0,0 +1,31 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+var ButtonToolbar = require('react-bootstrap').ButtonToolbar;
+var Button = require('react-bootstrap').Button;
+
+const buttonsInstance = (
+    <ButtonToolbar>
+        {/* Standard button */}
+        <Button>Default</Button>
+
+        {/* Provides extra visual weight and identifies the primary action in a set of buttons */}
+        <Button bsStyle="primary">Primary</Button>
+
+        {/* Indicates a successful or positive action */}
+        <Button bsStyle="success">Success</Button>
+
+        {/* Contextual button for informational alert messages */}
+        <Button bsStyle="info">Info</Button>
+
+        {/* Indicates caution should be taken with this action */}
+        <Button bsStyle="warning">Warning</Button>
+
+        {/* Indicates a dangerous or potentially negative action */}
+        <Button bsStyle="danger">Danger</Button>
+
+        {/* Deemphasize a button by making it look like a link while maintaining button behavior */}
+        <Button bsStyle="link">Link</Button>
+    </ButtonToolbar>
+);
+
+ReactDOM.render(buttonsInstance, document.getElementById('content'));
